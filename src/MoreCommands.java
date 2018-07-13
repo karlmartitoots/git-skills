02d19b355adc805c62b8fb7031d556994b8a3a5b
Karl Marti Toots <karlmarti65@gmail.com>
2018-07-13 19:37:10 +0300

    Added readme and some more basic commands

--- src/MoreCommands.java
+++ src/MoreCommands.java
@@ -1,5 +1,21 @@
 public class MoreCommands {
     public static void main(String[] args) {
+        //git branch <branchName>
+        //Create a new branch for separating development tasks
 
+        //git checkout <branchName>
+        //Switch to another branch, main branch is "master"
+
+        //git merge <branchName>
+        //Merge the branch with current working branch
+
+        //git remote
+        //List remote repositories
+
+        //git remote add <repoName> <repoURL>
+        //Add a remote repository
+
+        // git push -u <repoName> <branchName>
+        //Push changes in the branch to remote repository
     }
 }
--- src/OtherBranchCommands.java
+++ src/OtherBranchCommands.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
 public class OtherBranchCommands {
+    public static void main(String[] args) {
+
+    }
 }
