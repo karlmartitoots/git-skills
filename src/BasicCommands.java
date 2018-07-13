fd4a94c2301daa748d3027163b48a2ead1ccdb06
Karl Marti Toots <karlmarti65@gmail.com>
2018-07-13 18:55:28 +0300

    Initial commit

---
+++ src/BasicCommands.java
@@ -0,0 +1,28 @@
+public class BasicCommands {
+    public static void main(String[] args) {
+
+        //git init
+        //Initialize local git repository
+
+        //git add <file>
+        //Add file(s) to staging area of the index
+
+        //git status
+        //Check status of working tree
+
+        //git commit
+        //Commit changes in index to local repository
+
+        //git push
+        //Push to remote repository
+
+        //git pull
+        //Pull latest version from remote repository
+
+        //git clone
+        //Clone repository into a new directory
+
+        //git --version
+        //Gives current git version
+    }
+}
